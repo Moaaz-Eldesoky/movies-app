0bf53465371b344a082c3c6db9c6aa43a3129c22
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2023-09-06 17:01:49 +0300

    adding the register form with validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms'
+import {FormControl,FormGroup,Validator, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -8,10 +8,13 @@ import {FormControl,FormGroup} from '@angular/forms'
 })
 export class RegisterComponent {
   registerForm:FormGroup = new FormGroup({
-    first_name:new FormControl(null),
-    last_name:new FormControl(null),
-    age:new FormControl(null),
-    email:new FormControl(null),
-    password:new FormControl(null)
+    first_name:new FormControl(null, [Validators.required,Validators.minLength(3), Validators.maxLength(20)]),
+    last_name:new FormControl(null, [Validators.required,Validators.minLength(3), Validators.maxLength(20)]),
+    age:new FormControl(null, [Validators.required,Validators.min(16), Validators.max(30),Validators.pattern('^[0-9]*$')]),
+    email:new FormControl(null , [Validators.required,Validators.email]),
+    password:new FormControl(null,[Validators.required,Validators.pattern(`^[A-Za-z]\\w{7,15}$`)])
   })
+  submitRegsiterForm(registerForm:FormGroup){
+    console.log(registerForm);
+  }
 }
