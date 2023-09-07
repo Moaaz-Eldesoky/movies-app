fc1c65dc948e6dc484a48d62a6f3ea60077587a7
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2023-09-07 15:53:50 +0300

    add login form with its validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FormGroup,FormControl,Validator, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  loginForm:FormGroup = new FormGroup({
+    email:new FormControl(null , [Validators.required,Validators.email]),
+    password:new FormControl(null,[Validators.required,Validators.pattern(`^[A-Za-z]\\w{7,15}$`)])
+  })
+  submitLoginForm(loginForm:FormGroup){
+    console.log("hello");
+  }
 }
